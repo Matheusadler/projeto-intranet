f9bd87b975377ff5ee988c282b04655cf4bd7c07
Matheus Adler <Adler@imac-robert.mirante.com.br>
2020-03-04 13:01:46 -0300

    Listar Noticia

--- resources/views/admin/pages/add_noticia.blade.php
+++ resources/views/admin/pages/add_noticia.blade.php
@@ -234,13 +234,75 @@
                     <!-- /.card-body -->
                 </div>
                 <!-- /.card -->
+                <div class="card card-dark">
+                    <div class="card-header">
+                        <h3 class="card-title">Buscas</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <div class="form-group">
+                                <label>Tags</label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div>
+                        </div>
+                    </div>
 
+                    <!-- /.card-body -->
+                </div>
                 <!--/.col (right) -->
             </div>
             <!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
+    <div class="card-footer">
+        <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#ModalSubmit">Adicionar
+            Notícia</button>
+        <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#ModalCancel">Cancelar</button>
+    </div>
 
+    <!-- Modal Submit-->
+    <div class="modal fade" id="ModalSubmit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Adicionando Notícia</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Tem certeza que deseja adicionar esta notícia?
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-primary">Adicionar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Cancel-->
+    <div class="modal fade" id="ModalCancel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Cancelando Operação</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Tem certeza que deseja cancelar esta operação?
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-secondary" data-dismiss="modal">Quero continuar</button>
+                    <a href="/home" type="button" class="btn btn-primary swalDefaultSuccess">Sim</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <script src="{{ asset('ckeditor/ckeditor.js') }}"></script>
